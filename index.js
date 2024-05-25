5f64f4b2363195f6af74ea6487dfd467df1d3303
justbraf <dwaynecorey@gmail.com>
2024-05-24 21:38:55 -0400

    add doc

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { PORT, MongoDBURL } from './config.js'
 import { MongoClient, ObjectId, ServerApiVersion } from "mongodb"
+import { addDays, differenceInDays, format, subDays } from 'date-fns'
 const app = express()
 
 app.use(express.json())
@@ -23,13 +24,48 @@ app.listen(PORT, () => {
 })
 
 // Add a task (max length 160 characters)
+// Accepts task, dueDate, private, owner
 app.post('/add', (req, res) => {
     // get header data
-    // validate task length 160 char
-    // validate due date, if empty set next day
-    // set public, if private not set
+    const data = req.body
 
-    return res.status(200).send("Hi")
+    if (!data.task)
+        return res.status(200).send({ message: "No task found." })
+    // validate task length 160 char
+    if (data.task.length > 160)
+        return res.status(200).send({ message: "Task has too many characters." })
+    if (!data.owner)
+        return res.status(200).send({ message: "No owner found." })
+    // set public, if private not set
+    if (!data.private)
+        data.private = false
+    // validate due date, if empty set next day
+    if (!data.dueDate)
+        data.dueDate = format(addDays(Date.now(), 1), "yyyy-MM-dd")
+    // validate date format
+    if (differenceInDays(new Date(data.dueDate), Date.now()) < 0)
+        return res.status(200).send({ message: "Invalid date format, use: YYYY-MM-DD" })
+    else {
+        console.log("date: ", data.dueDate)
+        console.log("diff: ", differenceInDays(new Date(data.dueDate), Date.now()))
+        console.log("format: ", format(Date(data.dueDate), "yyyy-MM-dd"))
+        data.dueDate = format(Date(data.dueDate), "yyyy-MM-dd")
+    }
+    
+    // myTodos.insertOne(data)
+    //     .then(dbRes => {
+    //         let msg = {}
+    //         if (!dbRes.insertedId)
+    //             msg.message = "Oops! Something went wrong."
+    //         else
+    //             msg.message = "Added successfully."
+    //         return res.status(201).send(msg)
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
+    //         return res.status(201).send({ message: "Oops! Something went wrong." })
+    //     })
+    return res.status(201).send(data)
 })
 
 // Delete a task
@@ -40,11 +76,6 @@ app.delete('/remove', (req, res) => {
 
 // update task
 app.put('/update/:id', (req, res) => {
-    return res.status(200).send("Hi")
-})
-
-// update options
-app.put('/update/:id/:key', (req, res) => {
     return res.status(200).send("Hi")
 })
 
@@ -59,7 +90,10 @@ app.get('/tasks', (req, res) => {
                 msg.data = dbRes
             return res.status(200).send(msg)
         })
-        .catch(err => console.log(err))
+        .catch(err => {
+            console.log(err)
+            return res.status(201).send({ message: "Oops! Something went wrong." })
+        })
 })
 
 // get a task by id
@@ -75,5 +109,9 @@ app.get('/task/:id', (req, res) => {
             else
                 msg.data = dbRes
             return res.status(200).send(msg)
+        })
+        .catch(err => {
+            console.log(err)
+            return res.status(201).send({ message: "Oops! Something went wrong." })
         })
 })
